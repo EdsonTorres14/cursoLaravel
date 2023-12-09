bc6b4b2edcd85877e116178dacda1d8e7831d06f
Edson <edsontorresrios@gmail.com>
2023-12-08 20:38:52 -0600

    Video 212 - Llenando el formulario con la información de la vacante a editar

--- resources/views/livewire/editar-vacante.blade.php
+++ resources/views/livewire/editar-vacante.blade.php
@@ -71,22 +71,27 @@
         @enderror
     </div>
 
-    {{--  <div>
+    <div>
         <x-input-label for="imagen" :value="__('Imagen')" />
         <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen" accept="image/*" />
 
         <div class="my-5 w-80">
+            <x-input-label :value="__('Imagen Actual')" />
+
+            <img src="{{ asset('storage/vacantes/' . $imagen) }}" alt="{{ 'Imagen Vacante' . $titulo }}">
+        </div>
+
+        {{-- <div class="my-5 w-80">
             @if ($imagen)
                 Imagen:
                 <img src="{{ $imagen->temporaryUrl() }}">
             @endIf
-        </div>
+        </div> --}}
         @error('imagen')
             <livewire:mostrar-alerta :message="$message" />
         @enderror
     </div>
- --}}
     <x-primary-button class="justify-center">
-        {{ __('Crear Vacante') }}
+        {{ __('Guardar Cambios') }}
     </x-primary-button>
 </form>
